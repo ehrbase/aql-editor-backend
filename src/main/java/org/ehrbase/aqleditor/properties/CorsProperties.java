90561a9fde6d845b8e2ec3f3c436a8700ceb2c8f
Daniela Tomoiaga <daniela.tomoiaga@vitagroup.ag>
2021-01-04 15:09:20 +0200

    NUM-797: CORS configurable setting - allow all origins default

---
+++ src/main/java/org/ehrbase/aqleditor/properties/CorsProperties.java
@@ -0,0 +1,15 @@
+package org.ehrbase.aqleditor.properties;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "cors")
+public class CorsProperties {
+
+  private List<String> allowedOrigins = new ArrayList<>();
+}
