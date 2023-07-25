88a05f266b35923006fc9c2776795fde1dc68fac
stefan.spiska <stefan.spiska@vitagroup.ag>
2023-07-25 13:12:44 +0200

    add join from to resolve

--- src/main/java/org/ehrbase/aqleditor/service/FromService.java
+++ src/main/java/org/ehrbase/aqleditor/service/FromService.java
@@ -1,6 +1,8 @@
 package org.ehrbase.aqleditor.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 import lombok.AllArgsConstructor;
 import org.apache.commons.collections.iterators.ReverseListIterator;
@@ -8,8 +10,13 @@ import org.ehrbase.aqleditor.dto.containment.ResolveInput;
 import org.ehrbase.aqleditor.dto.containment.ResolveOutput;
 import org.ehrbase.openehr.sdk.aql.dto.containment.Containment;
 import org.ehrbase.openehr.sdk.aql.dto.containment.ContainmentClassExpression;
+import org.ehrbase.openehr.sdk.aql.dto.containment.ContainmentSetOperator;
+import org.ehrbase.openehr.sdk.aql.dto.containment.ContainmentSetOperatorSymbol;
 import org.ehrbase.openehr.sdk.aql.dto.operand.IdentifiedPath;
+import org.ehrbase.openehr.sdk.aql.dto.operand.StringPrimitive;
+import org.ehrbase.openehr.sdk.aql.dto.path.AndOperatorPredicate;
 import org.ehrbase.openehr.sdk.aql.dto.path.AqlObjectPath;
+import org.ehrbase.openehr.sdk.aql.dto.path.ComparisonOperatorPredicate;
 import org.ehrbase.openehr.sdk.aql.webtemplatepath.AqlPath;
 import org.ehrbase.openehr.sdk.webtemplate.model.WebTemplate;
 import org.ehrbase.openehr.sdk.webtemplate.model.WebTemplateNode;
@@ -54,6 +61,14 @@ public class FromService {
         fromLocatable.setType(next.getRmType());
         fromLocatable.setIdentifier(next.getRmType().toLowerCase().substring(0, 1) + "_" + random.nextInt(999));
 
+        fromLocatable.setPredicates(new ArrayList<>());
+        fromLocatable.getPredicates().add(new AndOperatorPredicate(new ArrayList<>()));
+        ComparisonOperatorPredicate e = new ComparisonOperatorPredicate();
+        e.setPath(AqlObjectPath.builder().node("archetype_node_id").build());
+        e.setOperator(ComparisonOperatorPredicate.PredicateComparisonOperator.EQ);
+        e.setValue(new StringPrimitive(next.getNodeId()));
+        fromLocatable.getPredicates().get(0).getOperands().add(e);
+
         fromEhr.setContains(fromLocatable);
 
         ResolveOutput resolveOutput = new ResolveOutput();
@@ -74,7 +89,62 @@ public class FromService {
     }
 
     private Containment join(Containment old, Containment add) {
+        if (old == null) {
+            return add;
+        }
 
-        return add;
+        if (old instanceof ContainmentClassExpression classExpression) {
+            ContainmentClassExpression addClassExpression1 = (ContainmentClassExpression) add;
+            if (matches(classExpression, addClassExpression1)) {
+
+                addClassExpression1.setIdentifier(classExpression.getIdentifier());
+
+                if (classExpression.getContains() != null && addClassExpression1.getContains() != null) {
+                    classExpression.setContains(join(classExpression.getContains(), addClassExpression1.getContains()));
+                }
+                return classExpression;
+            } else if (OrderContentmentTypes.isChild(classExpression.getType(), addClassExpression1.getType())) {
+
+                if (classExpression.getContains() != null) {
+
+                    classExpression.setContains(join(classExpression.getContains(), addClassExpression1));
+                    return classExpression;
+
+                } else {
+                    classExpression.setContains(add);
+                    return classExpression;
+                }
+            }
+        }
+
+        ContainmentSetOperator containmentSetOperator = new ContainmentSetOperator();
+        containmentSetOperator.setSymbol(ContainmentSetOperatorSymbol.AND);
+        containmentSetOperator.setValues(new ArrayList<>());
+        containmentSetOperator.getValues().add(old);
+        containmentSetOperator.getValues().add(add);
+
+        return containmentSetOperator;
+    }
+
+    private boolean matches(ContainmentClassExpression old, ContainmentClassExpression add) {
+
+        return old.getType().equals(add.getType()) && Objects.equals(extractAryctype(old), extractAryctype(add));
+    }
+
+    private String extractAryctype(ContainmentClassExpression classExpression) {
+
+        if (classExpression.getPredicates() == null) {
+            return null;
+        }
+
+        return classExpression.getPredicates().stream()
+                .map(o -> o.getOperands())
+                .flatMap(List::stream)
+                .filter(p -> p.getPath().getPathNodes().get(0).getAttribute().equals("archetype_node_id"))
+                .map(p -> p.getValue())
+                .map(p -> StringPrimitive.class.cast(p))
+                .map(StringPrimitive::getValue)
+                .findAny()
+                .orElse(null);
     }
 }
---
+++ src/main/java/org/ehrbase/aqleditor/service/OrderContentmentTypes.java
@@ -0,0 +1,34 @@
+package org.ehrbase.aqleditor.service;
+
+import java.util.List;
+
+/**
+ * @author Stefan Spiska
+ */
+public enum OrderContentmentTypes {
+    CLUSTER(),
+    OBSERVATION(CLUSTER),
+    EVALUATION(CLUSTER),
+    COMPOSITION(OBSERVATION, EVALUATION);
+
+    private final List<OrderContentmentTypes> children;
+
+    OrderContentmentTypes(OrderContentmentTypes... children) {
+        this.children = List.of(children);
+    }
+
+    public static boolean isChild(OrderContentmentTypes parent, OrderContentmentTypes child) {
+
+        if (parent.children.isEmpty()) {
+            return false;
+        } else {
+
+            return parent.children.stream().anyMatch(p -> isChild(p, child));
+        }
+    }
+
+    public static boolean isChild(String parentType, String childType) {
+
+        return isChild(OrderContentmentTypes.valueOf(parentType), OrderContentmentTypes.valueOf(childType));
+    }
+}
